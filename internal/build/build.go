9212738ae48d1b4530c3eeeda4d84f7c40dbac66
Johannes Tegnér <johannes@jitesoft.com>
2020-10-17 11:15:47 +0200

    Created a createTar function in build.go.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- internal/build/build.go
+++ internal/build/build.go
@@ -1,6 +1,8 @@
 package build
 
 import (
+	"archive/tar"
+	"bytes"
 	"fmt"
 	"github.com/go-pkg-org/gopkg/internal/control"
 	"io/ioutil"
@@ -56,6 +58,9 @@ func Build(directory string) error {
 
 func buildSourcePackage(directory, releaseVersion string, pkg control.Package) error {
 	pkgName := fmt.Sprintf("%s_%s.pkg", pkg.Package, releaseVersion)
+
+
+
 	cmd := exec.Command("tar", "-czvf", "build/"+pkgName, ".")
 	cmd.Dir = directory
 	cmd.Stdout = ioutil.Discard
@@ -91,4 +96,46 @@ func buildBinaryPackage(directory, releaseVersion, targetOs, targetArch string, 
 	fmt.Printf("Successfully build %s.pkg\n", pkgName)
 
 	return nil
+}
+
+// Create a tar file from a set of files.
+// The expected input `files` should be 'filename  => filePath' where the filename
+// is the path that will be created inside the tar file, and the filePath is the path
+// to the file to add.
+// The path is the path to the where the tar file will be created.
+func createTar(path string, files map[string]string, overwrite bool) error {
+	if !overwrite {
+		if _, err := ioutil.ReadFile(path); err != nil {
+			return fmt.Errorf("failed to create new tar source. File already exist")
+		}
+	}
+
+	var buffer bytes.Buffer
+	tw := tar.NewWriter(&buffer)
+
+	for fileName, file := range files {
+		fileBody, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		header := &tar.Header{
+			Name: fileName,
+			Mode: 0644,
+			Size: int64(len(fileBody)),
+		}
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+		if _, err := tw.Write(fileBody); err != nil {
+			return err
+		}
+	}
+
+	if err := tw.Close(); err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path, buffer.Bytes(), 0755)
 }
